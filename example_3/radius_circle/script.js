fc45ec62ccbec75bb514a25f0e4baf5b6d9c4f82
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-02-02 11:39:08 -0500

    Area of the circle

---
+++ example_3/radius_circle/script.js
@@ -0,0 +1,7 @@
+var radius = prompt("Enter the radius of the circle");
+
+function areaOfCircle(radius){
+    return(3.14 * (radius * radius))
+}
+
+alert('The area of the circle is '+ areaOfCircle(radius))
