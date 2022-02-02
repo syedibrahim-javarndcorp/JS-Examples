357393a83549f93208a7c0cb98bdfd8f90a7f36f
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-02-02 14:38:43 -0500

    form validation

---
+++ example_4/form_validation/script.js
@@ -0,0 +1,19 @@
+function syed() {
+  var x = document.forms["saveForm"]["fname"].value;
+  var y = document.forms["saveForm"]["lname"].value;
+  var z = document.forms["saveForm"]["phone"].value;
+  if (x == "") {
+    alert("First Name must be filled out");
+    document.saveForm.fname.focus();
+    return false;
+  } else if (y == "") {
+    alert("Last Name must be filled out");
+    document.saveForm.lname.focus();
+    return false;
+  } else if (z == "") {
+    alert("Phone number must be filled out");
+    document.saveForm.phone.focus();
+    return false;
+  } 
+  
+}
