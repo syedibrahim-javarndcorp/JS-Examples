f7f14f2f289951e11c96f847894f54c1cddcef71
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-02-03 09:59:04 -0500

    done using a api

---
+++ example_4/hit_counter/script.js
@@ -0,0 +1,13 @@
+// 
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/hit/hit-counter/mysite')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
