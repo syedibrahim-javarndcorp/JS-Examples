50d8f1fe5d786ab7d4d1888d440b849190b48c1f
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-02-04 08:42:58 -0500

    init

--- example_5/multiple_choices/script.js
+++ example_5/multiple_choices/script.js
@@ -16,9 +16,9 @@
                     s = s+ option.text + "\t \t" + price + "\n";
                 }
             }
-            for( var i = 0; i < starters.options.length; i++)
+            for( var i = 0; i < stocks.options.length; i++)
             {
-                var option = starters.options[i];
+                var option = stocks.options[i];
                 if(option.selected == true)
                 {
                     var price = parseInt(option.value);
